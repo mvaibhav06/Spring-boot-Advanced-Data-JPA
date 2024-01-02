40da61e588e654f31eca9479ea429dc5ad3cbe40
vaibhav <vaibhavmasetty06@gmail.com>
2024-01-02 18:28:13 -0500

    first commit

---
+++ src/main/java/luv2code/cruddemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package luv2code.cruddemo.dao;
+
+import luv2code.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+}
---
+++ src/main/java/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,25 @@
+package luv2code.cruddemo.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import luv2code.cruddemo.entity.Instructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+}
